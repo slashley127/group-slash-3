f3fed964aadcbe5d15c3b28bf1b393ead1333aee
jglynn1001 <joannaglynn@yahoo.com>
2024-01-15 18:58:56 -0600

    pushing spring

---
+++ src/test/java/com/slash3/travelapp/TravelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slash3.travelapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
