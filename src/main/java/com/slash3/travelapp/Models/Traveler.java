d8dc65c07e54b70d4f674e90940a666b41906c30
jglynn1001 <joannaglynn@yahoo.com>
2024-01-20 18:18:35 -0600

    Traveler model and User controller created

---
+++ src/main/java/com/slash3/travelapp/Models/Traveler.java
@@ -0,0 +1,15 @@
+package com.slash3.travelapp.Models;
+
+
+
+
+public class Traveler {
+
+    public int id;
+    public String name;
+    public String username;
+    public String email;
+    public String password;
+
+
+}
