6ed9dbb556c6b1d2012e7bae0f5fa353acca402c
jglynn1001 <joannaglynn@yahoo.com>
2024-01-24 16:04:36 -0600

    updated but still not connecting to MySQL

--- src/main/java/com/slash3/travelapp/Controllers/UserController.java
+++ src/main/java/com/slash3/travelapp/Controllers/UserController.java
@@ -1,24 +1,56 @@
 package com.slash3.travelapp.Controllers;
 
+import com.slash3.travelapp.Models.User;
+import com.slash3.travelapp.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class UserController {
+    @Autowired
+    private final UserRepository userrepository;
+
+    public UserController(UserRepository userrepository) {
+        this.userrepository = userrepository;
+
+    }
     @GetMapping
-    @ResponseBody
-    public String displayHomePage(){
-        return "WELCOME TO HOME PAGE! You can sign in or sign up!";
+
+    public List<User> getAllUsers() {
+        return (List<User>) userrepository.findAll();
     }
-    @GetMapping("/traveler")
-    @ResponseBody
-    public String displayTravelerPage(){
-        return "This is your Traveler Profile!";
+
+    @GetMapping("/{userId}")
+    public User getUserById(@PathVariable Integer userId) {
+        Optional<User> optionalUser = userrepository.findById(userId);
+        return optionalUser.orElse(null);
     }
-    @GetMapping("/createNewTraveler")
-    @ResponseBody
-    public String NewTravelerProfileForm(){
-        return "This is where you will create your traveler profile using a form!";
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userrepository.save(user);
+    }
+    @PostMapping ("/{userId}")
+    public User updateUser(Integer userId, User updatedUser) {
+        Optional<User> optionalUser = userrepository.findById(userId);
+
+        if (optionalUser.isPresent()) {
+            User existingUser = optionalUser.get();
+            existingUser.setUserName(updatedUser.getUserName());
+            existingUser.setEmail(updatedUser.getEmail());
+            existingUser.setPassword(updatedUser.getPassword());
+
+            return userrepository.save(existingUser);
+        }
+
+        return null;
+    }
+
+    public void deleteUser(Integer userId) {
+        userrepository.deleteById(userId);
     }
 }
