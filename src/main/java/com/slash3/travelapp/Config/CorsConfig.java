0b01814a5949170a2dcccfe73a695dd2bf6da072
jglynn1001 <joannaglynn@yahoo.com>
2024-02-05 13:38:20 -0600

    Activity Controller Added and mapping correctly. Many to Many added for Trip and Activity updated Config

--- src/main/java/com/slash3/travelapp/Config/CorsConfig.java
+++ src/main/java/com/slash3/travelapp/Config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.slash3.travelapp.config;
+package com.slash3.travelapp.Config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
