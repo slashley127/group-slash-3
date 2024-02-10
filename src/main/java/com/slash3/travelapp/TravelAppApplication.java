fe3aaa04d7b7d49b2d311228cb79fad5773a6688
jglynn1001 <joannaglynn@yahoo.com>
2024-02-09 23:25:59 -0600

    implemented CorsConfig file

--- src/main/java/com/slash3/travelapp/TravelAppApplication.java
+++ src/main/java/com/slash3/travelapp/TravelAppApplication.java
@@ -2,7 +2,10 @@ package com.slash3.travelapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.slash3.travelapp.Config.CorsConfig;
+import org.springframework.context.annotation.Import;
 
+@Import(CorsConfig.class)
 @SpringBootApplication
 public class TravelAppApplication {
 
