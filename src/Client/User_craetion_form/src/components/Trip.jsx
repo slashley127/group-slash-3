d537005f773dce083a94b345b577683a4349cb55
jglynn1001 <joannaglynn@yahoo.com>
2024-02-20 11:29:25 -0600

    Weather logic to display indoor vs outdoor div in place. Api call to activities not yet added. Further progress but not displaying yet

--- src/Client/User_craetion_form/src/components/Trip.jsx
+++ src/Client/User_craetion_form/src/components/Trip.jsx
@@ -5,7 +5,7 @@ import WeatherWidget from './WeatherWidget';
 function Trip() {
    const location = useLocation();
     const tripData = location.state && location.state.tripData;
-
+    const [activities, setActivities] = useState([]);
     const [weatherData, setWeatherData] = useState(null);
     const apiKey = '68cbf853b47ee9fec36896495fc006d2';
 
@@ -24,11 +24,36 @@ function Trip() {
            console.error('Error fetching weather data:', error);
          }
        };
+       const fetchActivities = async () => {
+             try {
+               const response = await fetch('/api/activities');
+               if (!response.ok) {
+                 throw new Error('Failed to fetch activities');
+               }
+               const data = await response.json();
+               setActivities(data);
+             } catch (error) {
+               console.error('Error fetching activities:', error);
+             }
+           };
 
        if (tripData) {
          fetchWeatherData();
+         fetchActivities();
        }
      }, [tripData]);
+
+     const filteredActivities = activities.filter(activity => {
+         if (weatherData && weatherData.main && weatherData.main.temp) {
+           if (weatherData.main.temp >= 40) {
+             return !activity.isIndoor; // Display outdoor activities if temperature is 40°F or warmer
+           } else {
+             return activity.isIndoor; // Display indoor activities if temperature is below 40°F
+           }
+         }
+         return false;
+       });
+
 
      return (
        <>
@@ -38,6 +63,12 @@ function Trip() {
              <p>Location: {tripData.tripLocation}</p>
              <p>Travelers: {tripData.traveler}</p>
              <WeatherWidget weatherData={weatherData} />
+             <h2>Recommended Activities:</h2>
+                       <ul>
+                         {filteredActivities.map(activity => (
+                           <li key={activity.id}>{activity.name}</li>
+                         ))}
+                       </ul>
            </>
          )}
        </>
