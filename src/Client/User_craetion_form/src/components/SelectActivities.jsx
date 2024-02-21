a0a67e5781cc7f6cf3f336f1da12e080177b206f
jglynn1001 <joannaglynn@yahoo.com>
2024-02-21 02:19:13 -0600

    Activities can now be selected for specific trips

--- src/Client/User_craetion_form/src/components/SelectActivities.jsx
+++ src/Client/User_craetion_form/src/components/SelectActivities.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 
-function SelectActivities({ tripId, activities}) {
+
+function SelectActivities({ tripId, activities }) {
     const [activityId, setActivityId] = useState('');
+    const [message, setMessage] = useState('');
 
     const handleAddActivityToTrip = async () => {
         try {
@@ -11,22 +13,23 @@ function SelectActivities({ tripId, activities}) {
             if (!response.ok) {
                 throw new Error('Failed to add activity to trip');
             }
-            // Handle success
+            setMessage('This activity was saved to your trip!');
         } catch (error) {
             console.error('Error adding activity to trip:', error);
+            setMessage('Failed to save activity to your trip');
         }
     };
 
     return (
                    <div>
-                       <h2>Select Activity to Add to Trip</h2>
-                       {/* Dropdown or list to select activities */}
+                       <h2>Select Local Activity to Add to Trip</h2>
                        <select value={activityId} onChange={(e) => setActivityId(e.target.value)}>
                            {activities.map(activity => (
                                <option key={activity.activityId} value={activity.activityId}>{activity.name}</option>
                            ))}
                        </select>
                        <button onClick={handleAddActivityToTrip}>Add Activity to Trip</button>
+                       <p>{message}</p>
                    </div>
                );
 }
