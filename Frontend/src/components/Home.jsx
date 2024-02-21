b495fb5d36fd5d684fab7986323f8b99f2414a6e
milhousecvh <135559562+milhousecvh@users.noreply.github.com>
2024-02-21 15:56:07 -0600

    simplifying home page view

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -7,17 +7,15 @@ const Home = () => {
   const [searchQuery, setSearchQuery] = useState('');
 
   return (
-    <section id='home' className='home'>
+         <section id='home' className='home'>
       <div className="overlay"></div>
-      <div data-aos="fade-down" className="homeContent container">
+      <div data-aos="fade-down" className="homeContent container text-center">
         <div className="textDiv">
           <span className="fst-italic">
-            Welcome to our Day Trip Planner
+            <h1>Welcome to our Day Trip Planner</h1>
           </span>
-          <h3 className="text-center bg-primary text-white my-1 py-1 rounded-lg shadow-lg  fst-italic">
-            Search for Your Perfect Holiday
-          </h3>
         </div>
+
         <div className="card-center bg-secondary text-white my-0 py-2">
           <div className="destinationInput">
             <label htmlFor="city" className="fst-italic">Search your destination:</label>
